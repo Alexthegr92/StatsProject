6b11d033341b56f56f708b054bfd027df00290a3
lozrobz13 <laurenroberts92@hotmail.co.uk>
2015-11-20 16:38:47 +0000

    finished off plotting the factors against income in the R files

--- data-modelling-frequentist.R
+++ data-modelling-frequentist.R
@@ -2,8 +2,16 @@
 mod1 = lm(Income~. , data=marketing)
 summary(mod1)
 
+# look at what anova does lol
 anova1 = aov(Income~., data=marketing)
 summary(anova1)
 
-plot(marketing$Sex, marketing$Income)
-plot(marketing$Marital, marketing$Income)
+plot(marketing$Sex, marketing$Income, main="sex")
+plot(marketing$Marital, marketing$Income, main="marital")
+plot(marketing$Occupation, marketing$Income, main="occupation")
+plot(marketing$Dual_Income, marketing$Income, main="dual income")
+plot(marketing$Status, marketing$Income, main="status")
+plot(marketing$Home_Type, marketing$Income, main="home type")
+plot(marketing$Ethnic, marketing$Income, main="ethnic")
+plot(marketing$Language, marketing$Income, main="language")
+
