ab9ce215f03c89814339bba65c9714f675fb4f25
lozrobz13 <laurenroberts92@hotmail.co.uk>
2015-11-20 15:12:18 +0000

    basic R files added

---
+++ data-tidying.R
@@ -0,0 +1,18 @@
+data("marketing")
+
+# change non ordered categorical varibales to factors
+# leave the others but maybe convert to continuous vars after looking at the data plots etc
+
+# omitted NAs
+marketing = na.omit(marketing)
+
+# make factors
+marketing$Sex = factor(marketing$Sex)
+marketing$Marital = factor(marketing$Marital)
+marketing$Occupation = factor(marketing$Occupation)
+marketing$Dual_Income = factor(marketing$Dual_Income)
+marketing$Status = factor(marketing$Status)
+marketing$Home_Type = factor(marketing$Home_Type)
+marketing$Ethnic = factor(marketing$Ethnic)
+marketing$Language = factor(marketing$Language)
+
